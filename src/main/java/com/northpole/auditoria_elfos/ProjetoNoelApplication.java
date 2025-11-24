8071383f1fe4d0b84043c6f2f52356bb84522106
dvaaz <darley.azevedo@gmail.com>
2025-11-24 18:52:32 -0300

    primeiro commit, atencao a versao do hibernate envers

---
+++ src/main/java/com/northpole/auditoria_elfos/ProjetoNoelApplication.java
@@ -0,0 +1,13 @@
+package com.northpole.auditoria_elfos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoNoelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoNoelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northpole/auditoria_elfos/ProjetoNoelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northpole.auditoria_elfos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoNoelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
